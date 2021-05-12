29b3c29701ad6b30a7865fa84842430bbcea6424
Javier Casanova Scoperetail <javier.casanova@scoperetail.com>
2021-05-12 16:29:17 -0500

    Auditor Listener

--- src/main/java/com/scoperetail/fusion/auditor/mapper/DomainEventMapper.java
+++ src/main/java/com/scoperetail/fusion/auditor/mapper/DomainEventMapper.java
@@ -2,15 +2,15 @@
 package com.scoperetail.fusion.auditor.mapper;
 
 import com.scoperetail.fusion.adapter.out.persistence.jpa.entity.MessageLogEntity;
-import com.scoperetail.fusion.auditor.dto.DomainEvent;
+import com.scoperetail.fusion.shared.kernel.events.DomainEvent;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface DomainEventMapper {
   @Mapping(target = "logKey", source = "event.eventId")
-  @Mapping(target = "eventId", source = "event.eventName")
-  @Mapping(target = "sourceTs", source = "event.sourceTs")
+  @Mapping(target = "eventId", source = "event.event")
+  @Mapping(target = "sourceTs", source = "event.timestamp")
   @Mapping(target = "payload", source = "event.payload")
   @Mapping(target = "createTs", source = "event.timestamp")
   MessageLogEntity getMessageLogEntity(DomainEvent event);
